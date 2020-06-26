f9a79f17df83e529a98892e845c59787620d9f49
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-26 03:33:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,16 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+full_name = {
+  first: "Jennifer"
+  last: "Yoo"
+}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  { :name => "Grace Hopper" }
 end
 
 
